d355fbd163e4c2697ed9e6151a90ed0f3648f647
Armando Rochin <armando.rochin@gmail.com>
2023-12-01 00:50:28 -0700

    Login design, Sign in design, Welcome design, color palette, font.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.armandorochin.firebaseshowcaseapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.armandorochin.firebaseshowcaseapp"
@@ -42,7 +42,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
